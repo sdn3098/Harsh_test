2d8e089800ce26ba9c047b0b73f0cd556f2eee84
Sumit Debnath <sdnatheh@gmail.com>
2021-07-04 16:38:47 +0530

    This is my first project

---
+++ src/main/java/test123/test1233/Test1233.java
@@ -0,0 +1,17 @@
+package test123.test1233;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Test1233 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
